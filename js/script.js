bd85d64311199b15636e593d1a9ba820a56cdac7
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-20 15:37:11 +0100

    created function to generate a random number not duplicated

--- js/script.js
+++ js/script.js
@@ -5,3 +5,19 @@ Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, 
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 */
 
+// per generare 5 numeri mi serve un ciclo che genera per 5 volte un numero casuale
+
+
+function genNumber(min, max){
+  let List = []; // array di elementi vuoto
+while(List.length < 5){ //condizione di uscita del ciclo
+  let num = getRndInteger(min, max); //funzione che serve per generare numeri casuali
+  if(!List.includes(num)){ // condizione di controllo se num non è compreso nella lista
+    List.push(num); // funzione per aggiungere num alla lista
+  }
+}
+return List; // mi ritornerà una lista di 5 elementi casuali diversi tra loro
+}
+
+const numList = genNumber(1, 100); // associo una variabile globale alla funzione genNumber()
+console.log(numList);
