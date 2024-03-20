92223f420eac153b7ea4d5d17b532566da4a9c96
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-03-20 18:43:36 +0100

    update

--- js/script.js
+++ js/script.js
@@ -7,35 +7,43 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 // per generare 5 numeri mi serve un ciclo che genera per 5 volte un numero casuale
 
-
-function genNumber(min, max){
+function genNumber(min, max) {
   let List = []; // array di elementi vuoto
-while(List.length < 5){ //condizione di uscita del ciclo
-  let num = getRndInteger(min, max); //funzione che serve per generare numeri casuali
-  if(!List.includes(num)){ // condizione di controllo se num non è compreso nella lista
-    List.push(num); // funzione per aggiungere num alla lista
+  while (List.length < 5) {
+    //condizione di uscita del ciclo
+    let num = getRndInteger(min, max); //funzione che serve per generare numeri casuali
+    if (!List.includes(num)) {
+      // condizione di controllo se num non è compreso nella lista
+      List.push(num); // funzione per aggiungere num alla lista
+    }
   }
-}
-return List; // mi ritornerà una lista di 5 elementi casuali diversi tra loro
+  return List; // mi ritornerà una lista di 5 elementi casuali diversi tra loro
 }
 const numList = genNumber(1, 100); // associo una variabile globale alla funzione genNumber()
 console.log(numList);
 
-
-function genBox(){ // creo una funzione per generare un elemento div con classe .box
-  const newEl = document.createElement('div')
+function genBox(content) {
+  // creo una funzione per generare un elemento div con classe .box
+  const newEl = document.createElement("div");
   // console.log(newEl);
-  newEl.classList.add('.box');
+  newEl.classList.add("box", "d-flex", "justify-content-center", "align-items-center", "mx-2");
   // console.log(newEl);
+  newEl.innerHTML = content;
 
   return newEl;
-};
-const newBox = genBox();
-console.log(newBox);
+}
 
-
-const play = document.getElementById('play'); // prendo il button dall'html tramite id
+const play = document.getElementById("play"); // prendo il button dall'html tramite id
 console.log(play);
-start.addEventListener('click', function(){ // associo un eventListener al bottone play
 
+// associo un eventListener al bottone play
+play.addEventListener("click", function () {
+  
+  const result = document.getElementById('result');
+  for (let i = 0; i < numList.length; i++) {
+    let newBox = genBox(numList[i]);
+    console.log(newBox);
+    result.append(newBox);
+
+  }
 });
